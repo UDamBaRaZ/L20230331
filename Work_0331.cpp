dc0b5b738bffc09d74f24d0bf8c600b329bce533
UDamBaRaZ <udambaraz@hanmail.net>
2023-04-03 08:14:46 +0900

    최종 수정

--- Work_0331.cpp
+++ Work_0331.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-
+#include <conio.h>
 
 using namespace std;
 
-
 int main()
 {
-	int Map[10][10] = 
-	{
+	//LevelDesign
+	int Map[10][10] = {
 		{1,1,1,1,1,1,1,1,1,1},
 		{1,0,0,0,0,0,0,0,0,1},
 		{1,0,0,0,0,0,0,0,0,1},
@@ -17,63 +16,65 @@ int main()
 		{1,0,0,0,0,0,0,0,0,1},
 		{1,0,0,0,0,0,0,0,0,1},
 		{1,0,0,0,0,0,0,0,0,1},
-		{1,1,1,1,1,1,1,1,1,1}
+		{1,1,1,1,1,1,1,1,1,1},
 	};
 
 	int PlayerX = 1;
 	int PlayerY = 1;
 	bool bIsRunning = true;
 
-
-	while (true) //GameLoop
+	while (bIsRunning) //GameLoop
 	{
 		//Frame
 		//Input();
-		 char Key;
-		 cin >> Key;
-
+		char Key;
+		Key = _getch();
 		//Tick();
-		 if (Key == 'd')
-		 {
-			 PlayerX++;
-		 }
-		 else if (Key == 'a')
-		 {
-			 PlayerX--;
-		 }
-		 else if (Key == 'w')
-		 {
-			 PlayerY++;
-		 }
-		 else if (Key == 's')
-		 {
-			 PlayerY--;
-		 }
-		 else if (Key == 'q')
-		 {
-			 bIsRunning = false;
-		 }
+		if (Key == 'd')
+		{
+			PlayerX++;
+		}
+		else if (Key == 'a')
+		{
+			PlayerX--;
+		}
+		else if (Key == 'w')
+		{
+			PlayerY--;
+		}
+		else if (Key == 's')
+		{
+			PlayerY++;
+		}
+		else if (Key == 'q')
+		{
+			bIsRunning = false;
+		}
 
 		//Render();
-		 system("cls");
+		system("cls");
 
 		for (int Y = 0; Y < 10; ++Y)
 		{
 			for (int X = 0; X < 10; ++X)
-				if (PlayerX == 1 && PlayerY == 1)
+			{
+				if (PlayerX == X && PlayerY == Y)
 				{
 					cout << "P";
 				}
-			else if (Map[Y][X] == 1)
-			{
-				cout << "#";
-			}
-			else if (Map[Y][X] == 0)
-			{
-				cout << "  ";
+				else if (Map[Y][X] == 1)
+				{
+					cout << "#";
+				}
+				else if (Map[Y][X] == 0)
+				{
+					cout << " ";
+				}
 			}
 			cout << endl;
-		}		
+		}
 	}
+
+
 	return 0;
 }
